7fba3565b41b8cce01ef486a16ed2abffbbb2757
Requena Hernandez <requenahdz@gmail.com>
2019-01-31 09:25:37 -0600

    carpeta en js

---
+++ js/firebase.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/5.8.1/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCaU5TKaRincTVldEJ5sFHAaQx16Uz6SQM",
+    authDomain: "fir-8cfd6.firebaseapp.com",
+    databaseURL: "https://fir-8cfd6.firebaseio.com",
+    projectId: "fir-8cfd6",
+    storageBucket: "",
+    messagingSenderId: "746254766091"
+  };
+  firebase.initializeApp(config);
+</script>

a28b14e43d882713a450204fd41f7a1e02983118
Requena Hernandez <requenahdz@gmail.com>
2019-02-05 09:56:08 -0600

    Add files via upload

---
+++ asset/firebase/init.js
@@ -0,0 +1,12 @@
+// Recuerda tener incluido estos js en el archivo html 
+// <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script> 
+// <script src="https://www.gstatic.com/firebasejs/5.8.1/firebase.js"></script>
+var config = {
+    apiKey: "AIzaSyCaU5TKaRincTVldEJ5sFHAaQx16Uz6SQM",
+    authDomain: "fir-8cfd6.firebaseapp.com",
+    databaseURL: "https://fir-8cfd6.firebaseio.com",
+    projectId: "fir-8cfd6",
+    storageBucket: "",
+    messagingSenderId: "746254766091"
+ };
+ firebase.initializeApp(config);
